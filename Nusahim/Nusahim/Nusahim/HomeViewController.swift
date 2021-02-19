d4a0136e30c3072d6d485616ad23e193bd0f783d
mashael marzoog <mashaelmarzoog@yahoo.com>
2021-02-19 17:08:25 +0300

    Login screen

---
+++ Nusahim/Nusahim/Nusahim/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Nusahim
+//
+//  Created by mashael marzoog on 19/02/2021.
+//  Copyright © 2021 SHAHD HAKEEM. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
